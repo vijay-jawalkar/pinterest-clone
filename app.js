5ff91707ecc87e5b66b9f8a66ecf95063e9fc2d2
vijay-jawalkar <vijay.jawalkar.786@gmail.com>
2024-01-20 10:34:05 +0530

    modified app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.set('trust proxy', 1);
+
 
 // it will create session and save data on server
 app.use(expressSession({
